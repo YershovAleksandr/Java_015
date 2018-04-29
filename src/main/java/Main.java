87d8a82ca8ecd382a20c196d360cc40b5bb60c38
hp <namstudionsk@gmail.com>
2018-04-29 22:22:46 +0700

    Add gitignore

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import java.io.PrintWriter;
+
+public class Main {
+    public static void main(String[] args){
+        PrintWriter pw = new PrintWriter(System.out, true);
+
+        pw.println("Хеллоу PIDOR Bitch!!");
+    }
+}
