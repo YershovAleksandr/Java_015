ea457f6cb1e8b2b2220dabd3f78d85fa2572d383
hp <namstudionsk@gmail.com>
2018-04-30 14:56:34 +0700

    Add staff

---
+++ src/main/java/Mainfoo.java
@@ -0,0 +1,13 @@
+import java.io.PrintWriter;
+
+public class Mainfoo {
+    public static void main(String... args){
+        PrintWriter pw = new PrintWriter(System.out, true);
+
+        pw.println("Pidor Bitch!!");
+
+
+
+
+    }
+}
